9be0ef5dd9c2e539e889887e5b1e53ac82a95450
apr-5 <j.hwa9368@gmail.com>
2025-02-16 22:38:17 +0900

    ✨feat: swagger 수정

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tmewzx@z6hy&+di(zg#9fd$*zdwti@y8%b#9k7!lpjlz%q_jfd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "13.125.213.193",
